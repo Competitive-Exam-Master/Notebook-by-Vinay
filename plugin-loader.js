8b922db7a64b96998c5d3673ffe82798bd0b7781
Competitive-Exam-Master <vinayaddress@gmail.com>
2025-07-20 15:09:09 +0530

    Update plugin-loader.js

--- plugin-loader.js
+++ plugin-loader.js
@@ -4,6 +4,21 @@ export function registerPlugin(plugin) {
   pluginRegistry.push(plugin);
 }
 
-export function initPlugins(editorContext) {
-  pluginRegistry.forEach(plugin => plugin.setup(editorContext));
+export async function initPlugins(context) {
+  const pluginList = [
+    'plugin-toolbar.js',
+    'plugin-format.js',
+    'plugin-theme.js'
+  ];
+
+  for (const file of pluginList) {
+    try {
+      const module = await import(`./plugins/${file}`);
+      // Plugin auto-registers via registerPlugin()
+    } catch (err) {
+      console.error(`Failed to load ${file}:`, err);
+    }
+  }
+
+  pluginRegistry.forEach(plugin => plugin.setup(context));
 }
