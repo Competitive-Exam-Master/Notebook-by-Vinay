79cb254901ed6d51ba72cd56ecac3814981c41a2
Competitive-Exam-Master <vinayaddress@gmail.com>
2025-07-22 21:48:58 +0530

    Update addons.js

--- addons/addons.js
+++ addons/addons.js
@@ -1,8 +1,6 @@
 // addons/addons.js
-// This file contains a list of JavaScript filenames for your custom add-ons.
-// Each file listed here will be dynamically loaded by app.js.
 const addonList = [
-    "exampleAddon.js",      // An example add-on that inserts predefined content
-    "printAddon.js",        // NEW: Add the print add-on
-    "alignTextAddon.js"
+    "exampleAddon.js",
+    "printAddon.js",
+    "alignTextAddon.js" // Add this line
 ];
